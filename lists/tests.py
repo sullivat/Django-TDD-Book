5f03063f0eb4c5f097a6e4c7e1f19fae799ef184
Tim Sullivan <sullivan.timm@gmail.com>
2017-01-24 15:10:11 -0700

    Add handling of additional todo items

--- lists/tests.py
+++ lists/tests.py
@@ -20,7 +20,7 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
         # TODO: "Code smell: POST test is too long?"
     
     def test_only_saves_items_when_neccesary(self):
@@ -55,3 +55,16 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+class ListViewTest(TestCase):
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+    
