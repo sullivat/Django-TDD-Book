24d16a14563812c70f68465b7b3ab704935f2e33
Tim Sullivan <sullivan.timm@gmail.com>
2017-02-05 06:47:21 -0700

    change DB settings to use 'dj-database-url'

--- superlists/settings.py
+++ superlists/settings.py
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# Database settings for Heroku deployment
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
