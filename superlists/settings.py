0494bb437bd15e04162a5f09dc54702cb94cae44
Tim Sullivan <sullivan.timm@gmail.com>
2017-02-05 14:40:46 -0700

    changing DB settings

--- superlists/settings.py
+++ superlists/settings.py
@@ -76,6 +76,14 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 
 # Database settings for Heroku deployment
 # Update database configuration with $DATABASE_URL.
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
