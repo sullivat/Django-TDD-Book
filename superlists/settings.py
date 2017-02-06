a9db50062d1371e45ec102063512bbed967a5312
Tim Sullivan <sullivan.timm@gmail.com>
2017-02-05 22:18:13 -0700

    fix seetings to get static serving to work

--- superlists/settings.py
+++ superlists/settings.py
@@ -134,12 +134,12 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles'))
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILE_DIRS = (
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(PROJECT_ROOT, 'static')
 )
 
 # Simplified static file serving.
